3454fa7b8a5fdae37fef1638150667ead4be8bc9
Jason Guo <jason.ghzr@gmail.com>
2025-01-27 18:42:03 -0800

    fix some issues and complete the EditListing page

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import ForgotPassword from './pages/ForgotPassword';
 import CreateListing from './pages/CreateListing';
 import Listing from './pages/Listing';
 import Contact from './pages/Contact';
+import EditListing from './pages/EditListing';
 
 function App() {
   return (
@@ -56,6 +57,10 @@ function App() {
           <Route
             path="/create-listing"
             element={<CreateListing />}
+          />
+          <Route
+            path="/edit-listing/:listingId"
+            element={<EditListing />}
           />
           <Route
             path="/category/:categoryName/:listingId"
